268a9a05c83add9e66c72e7586f6846b363333ca
terrymuleja <thierrynsaka@gmail.com>
2025-11-11 07:07:02 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body>{children}</body>
     </html>
   );
